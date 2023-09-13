80a84fb2b52a95f1f44f1c4d9266d09fa6b9c0a1
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-09-13 13:39:34 +0200

    create test before creating logic (TDD))

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,20 @@
+"""
+Models Testing
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestModel(TestCase):
+    """"Test models"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@example.com'
+        password = 'pass1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
