50ce230bc2fe069ebc3434a8d2a71e01b3e51984
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-09-13 18:39:38 +0200

    add user by admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,12 +4,45 @@ Django adming Customization
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
     """Define user admin pages"""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Personal info'),
+            {
+                'fields': ('name',)
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': ('last_login',)
+            }
+        )
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                # password1 and password2 have a predefined logic saved in django
+                'fields': ('email', 'password1',  'password2', 'name', 'is_active')
+            }
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,5 +31,19 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
         # assertContains checks if response contains certain item
+        self.assertEqual(res.status_code, 200)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test that edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that create user page works"""
+        url = reverse('admin:core_user_add')
+        print(url)
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
