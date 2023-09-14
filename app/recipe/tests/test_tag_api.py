e8cde2892a9ccbd0b369ad380e64e765104fce91
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-09-14 17:09:00 +0200

    fix linting

---
+++ app/recipe/tests/test_tag_api.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+
+def create_tag(user, name=None):
+    """Helper function to create new tag"""
+    if name is None:
+        name = 'Sample Tag'
+    return Tag.objects.create(user=user, name=name)
+
+
+# import pdb; pdb.set_trace()
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagTesting(TestCase):
+    """Test the publicly available tag API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagTesting(TestCase):
+    """Test the authorized user tag API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@example.com'
+            'testpass'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        create_tag(self.user)
+        create_tag(self.user, 'Test Tag')
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.data, serializer.data)
