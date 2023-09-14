63debe9965914dd6dd3fc598029bc279a2219936
omarfareed23 <omar.abdelaaty02@eng-st.cu.edu.eg>
2023-09-14 14:59:21 +0200

    add tags as many to many relation in the reciepes

--- app/core/models.py
+++ app/core/models.py
@@ -66,6 +66,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     description = models.TextField()
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
